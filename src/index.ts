cb1ed04bfe18152e9c37e8317ffbac690de5e0ee
Klaus <Klaus.Eckelt@jku.at>
2020-10-29 08:53:54 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import './style.scss'; // import styles as described https://github.com/webpack-contrib/sass-loader
+
+document.title = 'Boilerplate';
+document.getElementById('heading').textContent = '🔥 Boilerplate 🔥';
+console.log('Hello World')
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+// Taken from : https://webpack.js.org/guides/typescript/
+// plus:  https://github.com/webpack-contrib/sass-loader
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      { // typescript files
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }, 
+      { // sass files
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      }
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
