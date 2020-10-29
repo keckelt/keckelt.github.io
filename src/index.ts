37abe584521ecf7ba0464f3ec1271c0e79140d3d
Klaus Eckelt <Klaus.Eckelt@jku.at>
2020-10-29 10:47:05 +0100

    Initial Commit

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import './style.scss'; // import styles as described https://github.com/webpack-contrib/sass-loader
-
-document.title = 'Boilerplate';
-document.getElementById('heading').textContent = '🔥 Boilerplate 🔥';
-console.log('Hello World')
